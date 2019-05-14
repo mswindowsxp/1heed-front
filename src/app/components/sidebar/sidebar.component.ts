2870f4358492dc01ce0cc45e0f565cddd178c95b
hact <hact.esp@gmail.com>
2019-05-14 23:15:27 +0700

    Implement left side bar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
-    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
+  { path: '/dashboard', title: 'Hiển Thị Tất Cả', icon: 'ni-email-83 text-primary', class: '' },
+  { path: '/icons', title: 'Lọc Chưa Đọc', icon: 'ni-tag text-blue', class: '' },
+  { path: '/maps', title: 'Lọc Bình Luận', icon: 'ni-badge text-orange', class: '' },
+  { path: '/user-profile', title: 'Lọc Tin Nhắn', icon: 'ni-single-02 text-yellow', class: '' },
+  { path: '/tables', title: 'Lọc Đánh Giá', icon: 'ni-bullet-list-67 text-red', class: '' },
+  { path: '/login', title: 'Lọc Có Số Điện Thoại', icon: 'ni-tablet-button text-info', class: '' },
+  { path: '/register', title: 'Lọc Không Có Số Điện Thoại', icon: 'ni-fat-remove text-pink', class: '' },
+  { path: '/register', title: 'Lọc Chưa Trả Lời', icon: 'ni-bell-55 text-info', class: '' },
+  { path: '/register', title: 'Lọc Theo Khoảng Thời Gian', icon: 'ni-calendar-grid-58 text-orange', class: '' },
+  { path: '/register', title: 'Lọc Theo Bài Viết', icon: 'ni-paper-diploma text-yellow', class: '' },
+  { path: '/register', title: 'Lọc Theo Nhân Viên Được Phân Công', icon: 'ni-single-02 text-primary', class: '' }
 ];
 
 @Component({
@@ -23,16 +27,15 @@ export const ROUTES: RouteInfo[] = [
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
   public menuItems: any[];
   public isCollapsed = true;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
-    this.router.events.subscribe((event) => {
+    this.router.events.subscribe(event => {
       this.isCollapsed = true;
-   });
+    });
   }
 }
