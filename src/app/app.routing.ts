cc0ae341f347bc08714309b1d8971f37916511cf
HaCT1 <hact1@fsoft.com.vn>
2019-05-18 10:10:09 +0700

    Finish implement mock up left side bar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
@@ -10,8 +9,9 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'dashboard',
-    pathMatch: 'full',
-  }, {
+    pathMatch: 'full'
+  },
+  {
     path: '',
     component: AdminLayoutComponent,
     children: [
@@ -20,7 +20,8 @@ const routes: Routes = [
         loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
       }
     ]
-  }, {
+  },
+  {
     path: '',
     component: AuthLayoutComponent,
     children: [
@@ -29,19 +30,15 @@ const routes: Routes = [
         loadChildren: './layouts/auth-layout/auth-layout.module#AuthLayoutModule'
       }
     ]
-  }, {
+  },
+  {
     path: '**',
     redirectTo: 'dashboard'
   }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-  ],
+  imports: [CommonModule, BrowserModule, RouterModule.forRoot(routes)],
+  exports: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,17 +8,17 @@ declare interface RouteInfo {
   class: string;
 }
 export const ROUTES: RouteInfo[] = [
-  { path: '/dashboard', title: 'Hiển Thị Tất Cả', icon: 'ni-email-83 text-primary', class: '' },
-  { path: '/icons', title: 'Lọc Chưa Đọc', icon: 'ni-tag text-blue', class: '' },
-  { path: '/maps', title: 'Lọc Bình Luận', icon: 'ni-badge text-orange', class: '' },
-  { path: '/user-profile', title: 'Lọc Tin Nhắn', icon: 'ni-single-02 text-yellow', class: '' },
-  { path: '/tables', title: 'Lọc Đánh Giá', icon: 'ni-bullet-list-67 text-red', class: '' },
-  { path: '/login', title: 'Lọc Có Số Điện Thoại', icon: 'ni-tablet-button text-info', class: '' },
-  { path: '/register', title: 'Lọc Không Có Số Điện Thoại', icon: 'ni-fat-remove text-pink', class: '' },
-  { path: '/register', title: 'Lọc Chưa Trả Lời', icon: 'ni-bell-55 text-info', class: '' },
-  { path: '/register', title: 'Lọc Theo Khoảng Thời Gian', icon: 'ni-calendar-grid-58 text-orange', class: '' },
-  { path: '/register', title: 'Lọc Theo Bài Viết', icon: 'ni-paper-diploma text-yellow', class: '' },
-  { path: '/register', title: 'Lọc Theo Nhân Viên Được Phân Công', icon: 'ni-single-02 text-primary', class: '' }
+  { path: '/display-all', title: 'Hiển Thị Tất Cả', icon: 'ni-email-83 text-primary', class: '' },
+  { path: '/filter-by-unread', title: 'Lọc Chưa Đọc', icon: 'ni-tag text-blue', class: '' },
+  { path: '/filter-by-cmt', title: 'Lọc Bình Luận', icon: 'ni-badge text-orange', class: '' },
+  { path: '/filter-by-message', title: 'Lọc Tin Nhắn', icon: 'ni-single-02 text-yellow', class: '' },
+  { path: '/filter-by-vote', title: 'Lọc Đánh Giá', icon: 'ni-bullet-list-67 text-red', class: '' },
+  { path: '/filter-have-phone-no', title: 'Lọc Có Số Điện Thoại', icon: 'ni-tablet-button text-info', class: '' },
+  { path: '/filter-not-have-phone-no', title: 'Lọc Không Có Số Điện Thoại', icon: 'ni-fat-remove text-pink', class: '' },
+  { path: '/filter-not-reply', title: 'Lọc Chưa Trả Lời', icon: 'ni-bell-55 text-info', class: '' },
+  { path: '/filter-by-time', title: 'Lọc Theo Khoảng Thời Gian', icon: 'ni-calendar-grid-58 text-orange', class: '' },
+  { path: '/filter-by-post', title: 'Lọc Theo Bài Viết', icon: 'ni-paper-diploma text-yellow', class: '' },
+  { path: '/filter-by-empl', title: 'Lọc Theo Nhân Viên Được Phân Công', icon: 'ni-single-02 text-primary', class: '' }
 ];
 
 @Component({
