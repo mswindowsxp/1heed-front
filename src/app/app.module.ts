717b514294e679835530b5fd02f04b51256806be
HaCT1 <hact1@fsoft.com.vn>
2019-06-08 16:51:20 +0700

    Revert remove path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,54 @@
 import 'hammerjs';
 
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { MatIconModule, MatButtonModule } from '@angular/material';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
-import { FuseProgressBarModule } from '@fuse/components';
-import { TranslateModule } from '@ngx-translate/core';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { AppComponent } from 'app/app.component';
-import { fuseConfig } from 'app/fuse-config';
-import { LayoutModule } from 'app/layout/layout.module';
-import { AppStoreModule } from 'app/store/store.module';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {MatIconModule, MatButtonModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {FuseProgressBarModule} from '@fuse/components';
+import {TranslateModule} from '@ngx-translate/core';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {AppComponent} from 'app/app.component';
+import {fuseConfig} from 'app/fuse-config';
+import {LayoutModule} from 'app/layout/layout.module';
+import {AppStoreModule} from 'app/store/store.module';
 
-import { environment } from '../environments/environment';
-import { FuseSidebarModule } from './../@fuse/components/sidebar/sidebar.module';
-import { FuseThemeOptionsModule } from './../@fuse/components/theme-options/theme-options.module';
-import { FuseModule } from './../@fuse/fuse.module';
-import { FakeDbService } from './fake-db/fake-db.service';
-import { ApiModule, BASE_PATH, Configuration, ConfigurationParameters } from './http';
-import { AuthGuard } from './shared/guard/auth.guard';
-import { ShareModule } from './shared/share.module';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import { FuseSharedModule } from '@fuse/shared.module';
+import {environment} from '../environments/environment';
+import {FuseSidebarModule} from './../@fuse/components/sidebar/sidebar.module';
+import {FuseThemeOptionsModule} from './../@fuse/components/theme-options/theme-options.module';
+import {FuseModule} from './../@fuse/fuse.module';
+import {FakeDbService} from './fake-db/fake-db.service';
+import {ApiModule, BASE_PATH, Configuration, ConfigurationParameters} from './http';
+import {AuthGuard} from './shared/guard/auth.guard';
+import {ShareModule} from './shared/share.module';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
+import {FuseSharedModule} from '@fuse/shared.module';
 
 const appRoutes: Routes = [
     {
         path: 'apps',
         loadChildren: './main/apps/apps.module#AppsModule',
+        // canActivate: [AuthGuard]
+    },
+    {
+        path: 'pages',
+        loadChildren: './main/pages/pages.module#PagesModule',
+        // canActivate: [AuthGuard]
+    },
+    {
+        path: 'ui',
+        loadChildren: './main/ui/ui.module#UIModule'
+    },
+    {
+        path: 'documentation',
+        loadChildren:
+            './main/documentation/documentation.module#DocumentationModule'
+    },
+    {
+        path: 'angular-material-elements',
+        loadChildren:
+            './main/angular-material-elements/angular-material-elements.module#AngularMaterialElementsModule'
         // canActivate: [AuthGuard]
     },
     {
@@ -40,6 +60,13 @@ const appRoutes: Routes = [
         redirectTo: 'apps/dashboards/analytics'
     }
 ];
+
+function apiConfigFactory(): Configuration {
+    const params: ConfigurationParameters = {
+        basePath: environment.apiBasePath
+    };
+    return new Configuration(params);
+}
 
 @NgModule({
     declarations: [AppComponent],
@@ -66,14 +93,8 @@ const appRoutes: Routes = [
         MatButtonModule,
         FuseSharedModule
     ],
-    providers: [{ provide: BASE_PATH, useValue: environment.apiBasePath }],
+    providers: [{provide: BASE_PATH, useValue: environment.apiBasePath}],
     bootstrap: [AppComponent]
 })
-export class AppModule {}
-
-export function apiConfigFactory(): Configuration {
-    const params: ConfigurationParameters = {
-        basePath: environment.apiBasePath
-    };
-    return new Configuration(params);
+export class AppModule {
 }
