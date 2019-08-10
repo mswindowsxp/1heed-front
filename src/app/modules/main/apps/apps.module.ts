13998cffa57cabb4645e377616e4f134d5908d77
HaCT <hact.esp@gmail.com>
2019-08-10 13:39:19 +0700

    Remove useless component and class

--- src/app/modules/main/apps/apps.module.ts
+++ src/app/modules/main/apps/apps.module.ts
@@ -9,33 +9,12 @@ const routes = [
             './dashboards/analytics/analytics.module#AnalyticsDashboardModule'
     },
     {
-        path: 'dashboards/project',
-        loadChildren:
-            './dashboards/project/project.module#ProjectDashboardModule'
-    },
-    {
         path: 'chat',
         loadChildren: './chat/chat.module#ChatModule'
     },
     {
-        path: 'calendar',
-        loadChildren: './calendar/calendar.module#CalendarModule'
-    },
-    {
         path: 'e-commerce',
         loadChildren: './e-commerce/e-commerce.module#EcommerceModule'
-    },
-    {
-        path: 'todo',
-        loadChildren: './todo/todo.module#TodoModule'
-    },
-    {
-        path: 'contacts',
-        loadChildren: './contacts/contacts.module#ContactsModule'
-    },
-    {
-        path: 'scrumboard',
-        loadChildren: './scrumboard/scrumboard.module#ScrumboardModule'
     }
 ];
 
